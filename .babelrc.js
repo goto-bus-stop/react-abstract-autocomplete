2111d6d583d77884d2ea701825f8075820d5fcdd
Renée Kooi <renee@kooi.me>
2018-01-28 17:24:21 +0100

    Babel 7

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      modules: process.env.BABEL_ENV === 'testing' ? 'commonjs' : false,
+      loose: true,
+    }],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    ['babel-plugin-transform-react-remove-prop-types', { mode: 'wrap' }],
+  ],
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,6 @@
 import babel from 'rollup-plugin-babel';
 
-const fs = require('fs');
 const pkg = require('./package.json');
-
-const babelrc = JSON.parse(fs.readFileSync('.babelrc', 'utf8'));
-// Don't transform modules, so rollup can read them instead.
-babelrc.presets[0][1].modules = false;
-// Options for rollup-plugin-babelrc.
-Object.assign(babelrc, {
-  babelrc: false,
-  runtimeHelpers: true,
-});
 
 const externals = Object.keys(pkg.dependencies)
   .concat(Object.keys(pkg.peerDependencies));
@@ -22,7 +12,7 @@ export default {
     { format: 'es', file: 'index.es.js' },
   ],
   plugins: [
-    babel(babelrc),
+    babel(),
   ],
   external: id =>
     externals.some(external => id.split('/')[0] === external),
