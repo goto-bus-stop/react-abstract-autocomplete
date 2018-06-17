c54f70b6988eb387bf7b3a244a383119a9ee9715
Renée Kooi <renee@kooi.me>
2018-06-17 14:22:44 +0200

    Remove object rest spread plugin; included in preset-env.

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,6 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
     ['babel-plugin-transform-react-remove-prop-types', { mode: 'wrap' }],
   ],
 };
